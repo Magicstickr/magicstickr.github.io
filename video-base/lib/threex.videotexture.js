2816bf7b83a216a99f33bf1e8155e790b1cd979d
Yann Klein <yann.klein@me.com>
2018-11-19 09:51:49 -0800

    correction

--- video-base/lib/threex.videotexture.js
+++ video-base/lib/threex.videotexture.js
@@ -7,8 +7,7 @@ THREEx.VideoTexture	= function(url){
 	video.height	= 240;
 	video.autoplay	= true;
 	video.loop	= true;
-	video.setAttribute("WebKitPlaysInline", "true");
-	video.setAttribute("playslinline", "true");
+	video.setAttribute("playsinline", true);
 	video.src	= url;
 	// expose video as this.video
 	this.video	= video
