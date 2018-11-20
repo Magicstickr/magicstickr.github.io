e0ab5adcad5fde38c6ab4a8945721bd622b6bbb0
Yann Klein <yann.klein@me.com>
2018-11-19 16:39:54 -0800

    new video with poster

--- video-base/lib/threex.videotexture.js
+++ video-base/lib/threex.videotexture.js
@@ -1,13 +1,11 @@
 var THREEx = THREEx || {}
 
-THREEx.VideoTexture	= function(url, thumbnail){
+THREEx.VideoTexture	= function(url){
 	// create the video element
 	var video	= document.createElement('video');
 	video.width	= 320;
 	video.height	= 240;
 	video.setAttribute("playsinline", true);
-	video.setAttribute("poster", thumbnail);
-	video.setAttribute("controls", true);
 	video.src	= url;
 	// expose video as this.video
 	this.video	= video
