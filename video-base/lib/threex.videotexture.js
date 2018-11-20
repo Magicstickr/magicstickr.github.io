382fbf401e9251a40cc0b09de4d3b702dd65c6b4
Yann Klein <yann.klein@me.com>
2018-11-19 18:36:16 -0800

    thunmbnail again..

--- video-base/lib/threex.videotexture.js
+++ video-base/lib/threex.videotexture.js
@@ -1,10 +1,10 @@
 var THREEx = THREEx || {}
 
-THREEx.VideoTexture	= function(url){
+THREEx.VideoTexture	= function(url, thumbnail){
 	// create the video element
 	var video	= document.createElement('video');
 	video.width	= 320;
-	video.height	= 240;
+	video.height = 240;
 	video.setAttribute("playsinline", true);
 	video.setAttribute("poster", thumbnail);
 	video.src	= url;
