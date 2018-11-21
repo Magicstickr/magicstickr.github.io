c7e64bd3cf1d4a3b4b693fd6585e58ff2d454265
Yann Klein <yann.klein@me.com>
2018-11-21 14:26:55 -0800

    Add holo base

---
+++ holo-base/lib/chromakey.js
@@ -0,0 +1,54 @@
+ChromaKeyMaterial = function (url, width, height, keyColor) {
+	THREE.ShaderMaterial.call(this);
+
+	video = document.createElement('video');
+	video.loop = true;
+	video.src = url;
+	video.load();
+	video.play();
+
+	var videoImage = document.createElement('canvas');
+	if (window["webkitURL"]) document.body.appendChild(videoImage);
+	videoImage.width = width;
+	videoImage.height = height;
+	
+	var keyColorObject = new THREE.Color(keyColor);
+
+	var videoImageContext = videoImage.getContext('2d');
+	// background color if no video present
+	videoImageContext.fillStyle = '#' + keyColorObject.getHexString();
+	videoImageContext.fillRect(0, 0, videoImage.width, videoImage.height);
+
+	var videoTexture = new THREE.Texture(videoImage);
+	videoTexture.minFilter = THREE.LinearFilter;
+	videoTexture.magFilter = THREE.LinearFilter;
+
+	this.update = function () {
+		if (video.readyState === video.HAVE_ENOUGH_DATA) {
+			videoImageContext.drawImage(video, 0, 0);
+			if (videoTexture) {
+				videoTexture.needsUpdate = true;
+			}
+		}
+	}
+
+	this.setValues({
+
+		uniforms: {
+			texture: {
+				type: "t",
+				value: videoTexture
+			},
+			color: {
+				type: "c",
+				value: keyColorObject
+			}
+		},
+		vertexShader: document.getElementById('vertexShader').textContent,
+		fragmentShader: document.getElementById('fragmentShader').textContent,
+
+		transparent: true
+	});
+}
+
+ChromaKeyMaterial.prototype = Object.create(THREE.ShaderMaterial.prototype);
