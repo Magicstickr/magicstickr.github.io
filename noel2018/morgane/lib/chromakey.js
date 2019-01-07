eb1871a4ddb74d50e1a20ebdcef44bea1d1cd958
Yann Klein <yann.klein@me.com>
2019-01-06 17:18:33 -0800

    kill managers

--- noel2018/morgane/lib/chromakey.js
+++ noel2018/morgane/lib/chromakey.js
@@ -5,7 +5,7 @@ ChromaKeyMaterial = function (url, width, height, keyColor) {
 	video.loop = true;
 	video.src = url;
 	video.setAttribute("playsinline", ""); 
-	video.load(manager);
+	video.load();
 
 	var videoImage = document.createElement('canvas');
 	if (window["webkitURL"]) document.body.appendChild(videoImage);
