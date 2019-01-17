6d4d65d32bdd01f10db140193064e15ac9c86c17
Yann Klein <yann.klein@me.com>
2019-01-16 16:59:43 -0800

    cb project

--- cbproject/lib/ar-ms-kit1.0.js
+++ cbproject/lib/ar-ms-kit1.0.js
@@ -177,6 +177,8 @@ function initAREngine(markerPatt){
 	onRenderFcts.push(function(delta){
 		smoothedControls.update(markerRoot);
 	})
+
+	arWorldRoot = smoothedRoot;
 }
 
 function onDocumentTouchStart( event ) {
@@ -222,80 +224,234 @@ function addObjects(){
 	//		Add the objects in the scene
 	//////////////////////////////////////////////////////////////////////////////////
 
-	arWorldRoot = smoothedRoot;
-
 	var all = new THREE.Group();
 
-	// QR code plane
-	var geometry = new THREE.PlaneGeometry( 1.5, 1.5 );
-	var loader = new THREE.TextureLoader().load('content/logo.png', (imgLoader) => {
+	// Background plane
+	var geometry = new THREE.PlaneGeometry( 3, 5 );
+	var material = new THREE.MeshBasicMaterial( {color: 0xffffff, opacity: 0.5, transparent: true, side: THREE.DoubleSide} );
+	plane = new THREE.Mesh( geometry, material );
+	plane.position.set(1.5,3,0);
+	all.add( plane );
+
+	//strain title
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var xMid;
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {
+			color: color,
+			side: THREE.DoubleSide
+		} );
+		var message = "STRAIN: BLUE DREAM";
+		var shapes = font.generateShapes( message, 0.15 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		title = new THREE.Mesh( geometry, mat );
+		title.position.set(0.1,5,0.1);
+		all.add( title );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	//Text1
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {
+			color: color,
+			side: THREE.DoubleSide
+		} );
+		var message = "Blue Dream, a sativa-dominant hybrid originating\nin California, has achieved legendary status among\nWest Coast strains.";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		text1 = new THREE.Mesh( geometry, mat );
+		text1.position.set(0.1,4.8,0.1);
+		all.add( text1 );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	// California map
+	var geometry = new THREE.PlaneGeometry( 1, 1 );
+	var loader = new THREE.TextureLoader().load('content/california.png', (imgLoader) => {
 		});
 		//Load the image into a custom material
 		var material = new THREE.MeshLambertMaterial({
-		  map: loader,
+		  side: THREE.DoubleSide,
 		  transparent: true,
+		  map: loader,
 		});
-	qrPlane = new THREE.Mesh( geometry, material );
-	qrPlane.rotation.x = -Math.PI/2;
-	all.add( qrPlane );
+	button = new THREE.Mesh( geometry, material );
+	button.position.set(0.5,3.8,0.1);
+	all.add( button );
 
-	// Incredible Bulk Auto plane
-	var posX= 1;
-	var posY= 0;
-	var posZ= 1.3;
-	var geometry = new THREE.PlaneGeometry( 0.8, 0.7 );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00ffff, opacity: 0.0, transparent: true} );
-	icbPlane = new THREE.Mesh( geometry, material );
-	icbPlane.rotation.x = -Math.PI/2;
-	icbPlane.position.set(posX,posY - 0.2,posZ);
-	all.add( icbPlane );
+	//Happy
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {color: color,side: THREE.DoubleSide} );
+		var message = "HAPPY";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		happy = new THREE.Mesh( geometry, mat );
+		happy.position.set(0.1,3.2,0.1);
+		all.add( happy );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
 
-	var geometry = new THREE.CircleGeometry( 0.2 );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
-	icbCirc1 = new THREE.Mesh( geometry, material );
-	icbCirc1.rotation.x = -Math.PI/2;
-	icbCirc1.position.set(posX - 0.5,posY,posZ + 0.5);
-	icbCirc1.visible = false;
-	all.add( icbCirc1 );
-	icbCirc2 = new THREE.Mesh( geometry, material );
-	icbCirc2.rotation.x = -Math.PI/2;
-	icbCirc2.position.set(posX,posY,posZ + 0.6);
-	icbCirc2.visible = false;
-	all.add( icbCirc2 );
-	icbCirc3 = new THREE.Mesh( geometry, material );
-	icbCirc3.rotation.x = -Math.PI/2;
-	icbCirc3.position.set(posX + 0.5,posY,posZ + 0.5);
-	icbCirc3.visible = false;
-	all.add( icbCirc3 );
+	// Happy bar
+	var geometry = new THREE.PlaneGeometry( 2, 0.14 );
+	var material = new THREE.MeshLambertMaterial({ color: "#648B43",side: THREE.DoubleSide});
+	happy_bar = new THREE.Mesh( geometry, material );
+	happy_bar.position.set(2/2+0.8,3.25,0.1); 
+	all.add( happy_bar );
 
-	// GTA plane
-	var posX= 1;
-	var posY= 0;
-	var posZ= 2.7;
-	var geometry = new THREE.PlaneGeometry( 0.8, 0.7 );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00ffff, opacity: 0.0, transparent: true} );
-	gtaPlane = new THREE.Mesh( geometry, material );
-	gtaPlane.rotation.x = -Math.PI/2;
-	gtaPlane.position.set(posX,posY - 0.2,posZ);
-	all.add( gtaPlane );
+	//Relaxed
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {color: color,side: THREE.DoubleSide} );
+		var message = "RELAXED";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		relax = new THREE.Mesh( geometry, mat );
+		relax.position.set(0.1,3.05,0.1);
+		all.add( relax );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
 
-	var geometry = new THREE.CircleGeometry( 0.2 );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
-	gtaCirc1 = new THREE.Mesh( geometry, material );
-	gtaCirc1.rotation.x = -Math.PI/2;
-	gtaCirc1.position.set(posX - 0.5,posY,posZ + 0.5);
-	gtaCirc1.visible = false;
-	all.add( gtaCirc1 );
-	gtaCirc2 = new THREE.Mesh( geometry, material );
-	gtaCirc2.rotation.x = -Math.PI/2;
-	gtaCirc2.position.set(posX,posY,posZ + 0.6);
-	gtaCirc2.visible = false;
-	all.add( gtaCirc2 );
-	gtaCirc3 = new THREE.Mesh( geometry, material );
-	gtaCirc3.rotation.x = -Math.PI/2;
-	gtaCirc3.position.set(posX + 0.5,posY,posZ + 0.5);
-	gtaCirc3.visible = false;
-	all.add( gtaCirc3 );
+	// Relax bar
+	var geometry = new THREE.PlaneGeometry( 1.7, 0.14 );
+	var material = new THREE.MeshLambertMaterial({ color: "#648B43",side: THREE.DoubleSide});
+	relax_bar = new THREE.Mesh( geometry, material );
+	relax_bar.position.set(1.7/2+0.8,3.1,0.1);
+	all.add( relax_bar );
+
+	//Euphoric
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {color: color,side: THREE.DoubleSide} );
+		var message = "EUPHORIC";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		eupho = new THREE.Mesh( geometry, mat );
+		eupho.position.set(0.1,2.9,0.1);
+		all.add( eupho );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	// Eupho bar
+	var geometry = new THREE.PlaneGeometry( 1.5, 0.14 );
+	var material = new THREE.MeshLambertMaterial({ color: "#648B43",side: THREE.DoubleSide});
+	eupho_bar = new THREE.Mesh( geometry, material );
+	eupho_bar.position.set(1.5/2+0.8,2.95,0.1);
+	all.add( eupho_bar );
+
+	//Uplifted
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {color: color,side: THREE.DoubleSide} );
+		var message = "UPLIFTED";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		uplift = new THREE.Mesh( geometry, mat );
+		uplift.position.set(0.1,2.75,0.1);
+		all.add( uplift );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	// uplift bar
+	var geometry = new THREE.PlaneGeometry( 1.2, 0.14 );
+	var material = new THREE.MeshLambertMaterial({ color: "#648B43",side: THREE.DoubleSide});
+	uplift_bar = new THREE.Mesh( geometry, material );
+	uplift_bar.position.set(1.2/2+0.8,2.8,0.1);
+	all.add( uplift_bar );
+
+	//Creative
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {color: color,side: THREE.DoubleSide} );
+		var message = "CREATIVE";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		crea = new THREE.Mesh( geometry, mat );
+		crea.position.set(0.1,2.6,0.1);
+		all.add( crea );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	// crea bar
+	var geometry = new THREE.PlaneGeometry( 1.2, 0.14 );
+	var material = new THREE.MeshLambertMaterial({ color: "#648B43",side: THREE.DoubleSide});
+	crea_bar = new THREE.Mesh( geometry, material );
+	crea_bar.position.set(1.2/2+0.8,2.65,0.1);
+	all.add( crea_bar );
+
+	//Text2
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {
+			color: color,
+			side: THREE.DoubleSide
+		} );
+		var message = "With a sweet berry aroma redolent of its Blueberry\nparent, Blue Dream delivers swift symptom relief\nwithout heavy sedative effects. This makes Blue\nDream a popular daytime medicine for patients\ntreating pain, depression, nausea, and other\nailments requiring a high THC strain.";
+		var shapes = font.generateShapes( message, 0.09 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		text2 = new THREE.Mesh( geometry, mat );
+		text2.position.set(0.1,2.3,0.1);
+		all.add( text2 );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	//Order text
+	var loader = new THREE.FontLoader();
+	loader.load('lib/optimer_regular.typeface.json', function ( font ) {
+		var xMid;
+		var color = 0x000000;
+		var mat = new THREE.MeshBasicMaterial( {
+			color: color,
+			side: THREE.DoubleSide
+		} );
+		var message = "ORDER HERE";
+		var shapes = font.generateShapes( message, 0.15 );
+		var geometry = new THREE.ShapeBufferGeometry( shapes );
+		geometry.computeBoundingBox();
+		ordtext = new THREE.Mesh( geometry, mat );
+		ordtext.position.set(0.5,0.9,0.1);
+		all.add( ordtext );
+	}, undefined, function ( e ) {
+		console.error( e );
+	});
+
+	// Order button
+	var geometry = new THREE.CircleGeometry( 0.3, 32 );
+	var loader = new THREE.TextureLoader().load('content/button.jpg', (imgLoader) => {
+		});
+		//Load the image into a custom material
+		var material = new THREE.MeshLambertMaterial({
+		  side: THREE.DoubleSide,
+		  map: loader,
+		});
+	button = new THREE.Mesh( geometry, material );
+	button.position.set(2.2,1,0.1);
+	all.add( button );
 
 	// Load marijuana
 	var loader = new THREE.GLTFLoader().load('content/cb/scene.gltf', function ( gltf ) {
@@ -311,45 +467,25 @@ function addObjects(){
 		bbox.getSize(size);
 		gltf.scene.position.copy(cent).multiplyScalar(1);
 
-		gltf.scene.scale.multiplyScalar(4);
-		gltf.scene.position.set(1,0,1.3);
-		gltf.scene.visible = false;
+		gltf.scene.scale.multiplyScalar(3);
+		gltf.scene.position.set(-1.5,2,0);
+
+		onRenderFcts.push(function(){
+			gltf.scene.rotation.y += - 0.005*Math.PI;
+			//console.log(gltf.scene.rotation.y);
+		})
 	
-		marijuana1 = gltf.scene;
+		marijuana = gltf.scene;
 		//add it to the scene
-		all.add( marijuana1 );
+		all.add( marijuana );
 
 	}, undefined, function ( e ) {
 		console.error( e );
 	} );
 
-	// Load marijuana
-	var loader = new THREE.GLTFLoader().load('content/cb/scene.gltf', function ( gltf ) {
-		//Resize/rescale the 3D Object
-		var bbox = new THREE.Box3().setFromObject(gltf.scene);
-		var cent = bbox.getCenter(new THREE.Vector3());
-		var size = bbox.getSize(new THREE.Vector3());
-		//Rescale the object to normalized space
-		var maxAxis = Math.max(size.x, size.y, size.z);
-		gltf.scene.scale.multiplyScalar(1.0 / maxAxis);
-		bbox.setFromObject(gltf.scene);
-		bbox.getCenter(cent);
-		bbox.getSize(size);
-		gltf.scene.position.copy(cent).multiplyScalar(1);
 
-		gltf.scene.scale.multiplyScalar(4);
-		gltf.scene.position.set(1,0,2.7);
-		gltf.scene.visible = false;
-	
-		marijuana2 = gltf.scene;
-		//add it to the scene
-		all.add( marijuana2 );
 
-	}, undefined, function ( e ) {
-		console.error( e );
-	} );
-
-	all.scale.multiplyScalar(1);
+	all.scale.multiplyScalar(0.5);
 	//all.position.z = 0.5;
 	arWorldRoot.add(all);
 }
@@ -366,37 +502,12 @@ function onDocumentMouseDown( event ) {
 
 	raycaster.setFromCamera( mouse, camera );
 
-	var icbArray = [icbPlane];
-	var gtaArray = [gtaPlane];
+	var array = [button, ordtext];
 
-	var icbIntersects = raycaster.intersectObjects( icbArray );
-	var gtaIntersects = raycaster.intersectObjects( gtaArray );
+	var intersects = raycaster.intersectObjects( array );
 
-	if ( icbIntersects.length > 0) {
-		if(marijuana1.visible == false){
-			marijuana1.visible = true;
-			icbCirc1.visible = true;
-			icbCirc2.visible = true;
-			icbCirc3.visible = true;;
-		}else{
-			marijuana1.visible = false;
-			icbCirc1.visible = false;
-			icbCirc2.visible = false;
-			icbCirc3.visible = false;
-		}
-	}
-	if ( gtaIntersects.length > 0) {
-		if(marijuana2.visible == false){
-			marijuana2.visible = true;
-			gtaCirc1.visible = true;
-			gtaCirc2.visible = true;
-			gtaCirc3.visible = true;
-		}else{
-			marijuana2.visible = false;
-			gtaCirc1.visible = false;
-			gtaCirc2.visible = false;
-			gtaCirc3.visible = false;
-		}
+	if ( intersects.length > 0) {
+		window.location.href = 'https://www.leafly.ca/hybrid/blue-dream';
 	}
 }
 
@@ -511,6 +622,24 @@ function initSequence(){
 		}
 
 	})
+}
+
+function initControl(){
+	// controls
+
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+	//controls.addEventListener( 'change', render ); // call this only in static arWorldRoots (i.e., if there is no animation loop)
+
+	controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+	controls.dampingFactor = 0.25;
+
+	controls.screenSpacePanning = false;
+
+	controls.minDistance = 0;
+	controls.maxDistance = 500;
+
+	controls.maxPolarAngle = Math.PI / 2;
 }
 
 function test(test_var){
