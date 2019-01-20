e8e5f7bbd92b08a4d04274a7253796812b42bf08
Yann Klein <yann.klein@me.com>
2019-01-20 13:07:39 -0800

    changes

--- noel2018/lib/ar-ms-kit1.0.js
+++ noel2018/lib/ar-ms-kit1.0.js
@@ -74,7 +74,8 @@ function initScene(){
 		autoResize : true,
 		alpha: true
 	});
-	renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+	renderer.setClearColor(new THREE.Color('lightgrey'), 0);
+	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.domElement.style.position = 'absolute'
 	renderer.domElement.style.top = '0px'
